95327f2ad023ec5268b47c453d6272be649b3505
Andre Smith <smitti24@gmail.com>
2020-05-11 11:40:07 +0200

    Commit

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        // Automatically adds Id as an identity column.
+        [Key]
+        public int Id { get; set; }
+
+        // Name cannot be null
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CreateModel(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
